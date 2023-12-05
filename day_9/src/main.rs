f35580d5f9a7ea724d61fadcf525ee8ea21c9e3f
Banaantje04 <banaantje27@outlook.com>
2023-12-05 22:46:53 +0100

    Template for the rest because i am lazy

---
+++ day_9/src/main.rs
@@ -0,0 +1,15 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn solve_part_1() -> usize {
+    0
+}
+
+fn solve_part_2() -> usize {
+    0
+}
+
+fn main() {
+    println!("Day 9:");
+    println!("The solution to part one is: {}", solve_part_1());
+    println!("The solution to part two is: {}", solve_part_2());
+}
