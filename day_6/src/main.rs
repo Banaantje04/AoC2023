231e4e109c1a3b72121246a82853e0e6dd8d1c44
Banaantje04 <banaantje27@outlook.com>
2023-12-06 11:08:47 +0100

    Day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,15 +1,42 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn solve_part_1() -> usize {
-    0
+    let times = INPUT.lines().next().unwrap().split_whitespace().skip(1).map(|t| t.parse().unwrap()).collect::<Vec<usize>>();
+    let distances = INPUT.lines().skip(1).next().unwrap().split_whitespace().skip(1).map(|d| d.parse().unwrap()).collect::<Vec<usize>>();
+
+    let mut sum = 1;
+    for (i, time) in times.iter().enumerate() {
+        let distance = distances[i];
+        let mut num = 0;
+        for j in 1..*time {
+            if (*time - j)*j > distance {
+                num += 1;
+            }
+        }
+        sum *= num;
+    }
+    sum
 }
 
 fn solve_part_2() -> usize {
-    0
+    let mut time = INPUT.lines().next().unwrap().to_owned();
+    time.retain(|c| c.is_digit(10));
+    let time = time.parse::<usize>().unwrap();
+    let mut distance = INPUT.lines().skip(1).next().unwrap().to_owned();
+    distance.retain(|c| c.is_digit(10));
+    let distance = distance.parse::<usize>().unwrap();
+
+    let mut num = 0;
+    for j in 1..time {
+        if (time - j)*j > distance {
+            num += 1;
+        }
+    }
+    num
 }
 
 fn main() {
-    println!("Day 6:");
+    println!("Day 6: Wait For It");
     println!("The solution to part one is: {}", solve_part_1());
     println!("The solution to part two is: {}", solve_part_2());
 }
