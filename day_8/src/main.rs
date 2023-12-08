a78906f0a4e028cac5486ca2ed323b321e6e7d57
Banaantje04 <banaantje27@outlook.com>
2023-12-08 17:00:01 +0100

    Cleanup

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -3,116 +3,79 @@ use num::integer::lcm;
 const INPUT: &str = include_str!("input.txt");
 
 fn solve_part_1() -> usize {
-    let mut input_l = INPUT.lines();
-    let moves = input_l.next().unwrap();
+    let (graph, moves) = create_graph();
 
-    let mut graph = Graph { nodes: Vec::new() };
-    let mut node_list = Vec::new();
-    for n in input_l.skip(1) {
-        let mut line_split = n.split(" = ");
-        let id = line_split.next().unwrap();
-        let dirs = line_split.next().unwrap();
-        let mut dirs = (&dirs[1..dirs.len() - 1]).split(", ");
-        let left = dirs.next().unwrap();
-        let right = dirs.next().unwrap();
-
-        graph.nodes.push(Node {
-            id,
-            left: 0,
-            right: 0,
-        });
-        node_list.push((id, (left, right)));
-    }
-
-    for n in node_list {
-        let left = graph.nodes.iter().position(|l| l.id == n.1 .0).unwrap();
-        let right = graph.nodes.iter().position(|r| r.id == n.1 .1).unwrap();
-
-        let node: &mut Node = graph.nodes.iter_mut().find(|i| i.id == n.0).unwrap();
-        node.left = left;
-        node.right = right;
-    }
-
-    let mut curr = graph.nodes.iter().find(|n| n.id == "AAA").unwrap();
-    let mut movechar = moves.chars();
-    let mut num = 0;
-    while curr.id != "ZZZ" {
-        match movechar.next() {
-            Some(dir) => {
-                num += 1;
-                match dir {
-                    'R' => curr = &graph.nodes[curr.right],
-                    'L' => curr = &graph.nodes[curr.left],
-                    c => panic!("This is not a direction! {c}"),
-                }
-            }
-            None => movechar = moves.chars(),
-        }
-    }
-
-    num
+    get_runtime(&graph, graph.nodes.iter().find(|i| i.id == "AAA").unwrap(), moves)
 }
 
 fn solve_part_2() -> usize {
-    let mut input_l = INPUT.lines();
-    let moves = input_l.next().unwrap();
-
-    let mut graph = Graph { nodes: Vec::new() };
-    let mut node_list = Vec::new();
-    for n in input_l.skip(1) {
-        let mut line_split = n.split(" = ");
-        let id = line_split.next().unwrap();
-        let dirs = line_split.next().unwrap();
-        let mut dirs = (&dirs[1..dirs.len() - 1]).split(", ");
-        let left = dirs.next().unwrap();
-        let right = dirs.next().unwrap();
-
-        graph.nodes.push(Node {
-            id,
-            left: 0,
-            right: 0,
-        });
-        node_list.push((id, (left, right)));
-    }
-
-    for n in node_list {
-        let left = graph.nodes.iter().position(|l| l.id == n.1 .0).unwrap();
-        let right = graph.nodes.iter().position(|r| r.id == n.1 .1).unwrap();
-
-        let node: &mut Node = graph.nodes.iter_mut().find(|i| i.id == n.0).unwrap();
-        node.left = left;
-        node.right = right;
-    }
+    let (graph, moves) = create_graph();
 
     let curr = graph.nodes.iter().filter(|n| n.id.ends_with("A"));
-    let runtimes = curr.map(|n| {
-        let mut curr_n = n;
-        let mut movechar = moves.chars();
-        let mut num = 0;
-        while !curr_n.id.ends_with("Z") {
-            match movechar.next() {
-                Some(dir) => {
-                    num += 1;
-                    match dir {
-                        'R' => curr_n = &graph.nodes[curr_n.right],
-                        'L' => curr_n = &graph.nodes[curr_n.left],
-                        c => panic!("This is not a direction! {c}"),
-                    }
-                }
-                None => movechar = moves.chars(),
-            }
-        }
-        num
-    });
+    let runtimes = curr.map(|n| get_runtime(&graph, n, moves));
     
-    println!("{:?}", runtimes.clone().collect::<Vec<_>>());
-    runtimes.reduce(|lcm, n| num::integer::lcm(lcm, n)).unwrap()
+    //println!("{:?}", runtimes.clone().collect::<Vec<_>>());
+    runtimes.reduce(|acc, n| lcm(acc, n)).unwrap()
 }
 
 fn main() {
     println!("Day 8: Haunted Wasteland");
     println!("The solution to part one is: {}", solve_part_1());
     println!("The solution to part two is: {}", solve_part_2());
+}
+
+fn create_graph<'a>() -> (Graph<'a>, &'static str) {
+    let mut input_l = INPUT.lines();
+    let moves = input_l.next().unwrap();
+
+    let mut graph = Graph { nodes: Vec::new() };
+    let mut node_list = Vec::new();
+    for n in input_l.skip(1) {
+        let mut line_split = n.split(" = ");
+        let id = line_split.next().unwrap();
+        let dirs = line_split.next().unwrap();
+        let mut dirs = (&dirs[1..dirs.len() - 1]).split(", ");
+        let left = dirs.next().unwrap();
+        let right = dirs.next().unwrap();
+
+        graph.nodes.push(Node {
+            id,
+            left: 0,
+            right: 0,
+        });
+        node_list.push((id, (left, right)));
+    }
+
+    for n in node_list {
+        let left = graph.nodes.iter().position(|l| l.id == n.1 .0).unwrap();
+        let right = graph.nodes.iter().position(|r| r.id == n.1 .1).unwrap();
+
+        let node: &mut Node = graph.nodes.iter_mut().find(|i| i.id == n.0).unwrap();
+        node.left = left;
+        node.right = right;
+    }
+
+    (graph, moves)
+}
+
+fn get_runtime(graph: &Graph, start_node: &Node, moves: &str) -> usize {
+    let mut curr_n = start_node;
+    let mut movechar = moves.chars();
+    let mut num = 0;
+    while !curr_n.id.ends_with("Z") {
+        match movechar.next() {
+            Some(dir) => {
+                num += 1;
+                match dir {
+                    'R' => curr_n = &graph.nodes[curr_n.right],
+                    'L' => curr_n = &graph.nodes[curr_n.left],
+                    c => panic!("This is not a direction! {c}"),
+                }
+            }
+            None => movechar = moves.chars(),
+        }
+    }
+    num
 }
 
 struct Graph<'a> {
