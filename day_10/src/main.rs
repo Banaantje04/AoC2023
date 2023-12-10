29fdc229cd41bc2b3974d1230442c1004a9f511b
Banaantje04 <banaantje27@outlook.com>
2023-12-10 17:08:56 +0100

    Part 2 done

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap,HashSet};
 
 const INPUT: &str = include_str!("input.txt");
 
@@ -17,7 +17,7 @@ fn solve_part_1() -> usize {
     }
 
     let directions = get_directions(&tiles, start);
-    println!("The position of the start is: {:?} with directions: {:?}", start, directions);
+    //println!("The position of the start is: {:?} with directions: {:?}", start, directions);
 
     let mut visited = HashMap::new();
     let mut prev = start;
@@ -26,7 +26,7 @@ fn solve_part_1() -> usize {
     while current != start {
         visited.insert(current, distance);
         let tmp = current;
-        current = get_next(&tiles, prev, current);
+        (current, _) = get_next(&tiles, prev, current);
         prev = tmp;
         distance += 1;
     }
@@ -36,7 +36,7 @@ fn solve_part_1() -> usize {
     prev = start;
     while distance != *visited.get(&current).unwrap_or(&0) {
         let tmp = current;
-        current = get_next(&tiles, prev, current);
+        (current, _) = get_next(&tiles, prev, current);
         prev = tmp;
         distance += 1;
     }
@@ -59,18 +59,52 @@ fn solve_part_2() -> usize {
     }
 
     let directions = get_directions(&tiles, start);
-    println!("The position of the start is: {:?} with directions: {:?}", start, directions);
+    //println!("The position of the start is: {:?} with directions: {:?}", start, directions);
 
+    let mut visited = HashSet::new();
+    visited.insert(start);
     let mut prev = start;
     let mut current = directions.0;
     while current != start {
+        visited.insert(current);
         let tmp = current;
-        current = get_next(&tiles, prev, current);
+        (current, _) = get_next(&tiles, prev, current);
         prev = tmp;
-        tiles[tmp.0][tmp.1] = 'V';
     }
 
-    let string = tiles.iter().map(|l| l.iter().map(|c| c.to_string()).reduce(|acc, c| {
+    prev = start;
+    current = directions.0;
+    while current != start {
+        visited.insert(current);
+        let tmp = current;
+        let dir;
+        (current, dir) = get_next(&tiles, prev, current);
+        prev = tmp;
+
+        let mut current_inside = get_next_left_of_dir(prev, dir);
+        while !visited.contains(&current_inside) {
+            //println!("{}", to_string(&tiles));
+            //println!("");
+            tiles[current_inside.0][current_inside.1] = 'I';
+            current_inside = get_next_left_of_dir(current_inside, dir);
+        }
+        current_inside = get_next_left_of_dir(current, dir);
+        while !visited.contains(&current_inside) {
+            //println!("{}", to_string(&tiles));
+            //println!("");
+            tiles[current_inside.0][current_inside.1] = 'I';
+            current_inside = get_next_left_of_dir(current_inside, dir);
+        }
+    }
+
+    //println!("{}", to_string(&tiles));
+
+
+    tiles.iter().flatten().filter(|c| **c == 'I').count()
+}
+
+fn to_string(tiles: &Vec<Vec<char>>) -> String {
+    tiles.iter().map(|l| l.iter().map(|c| c.to_string()).reduce(|acc, c| {
         let mut str = acc.clone();
         str.push_str(&c);
         str
@@ -79,33 +113,29 @@ fn solve_part_2() -> usize {
         str.push('\n');
         str.push_str(&l);
         str
-    }).unwrap();
-    println!("{}", string);
-    
-    0
+    }).unwrap()
 }
 
 fn get_directions(tiles: &Vec<Vec<char>>, position: (usize, usize)) -> ((usize, usize), (usize, usize)) {
-    let position_val = tiles[position.0][position.1];
-    let north = if position.0 > 0 {tiles[position.0-1][position.1]} else {position_val};
-    let east = if position.1 < tiles[0].len() - 1 {tiles[position.0][position.1+1]} else {position_val};
-    let south = if position.0 < tiles.len() - 1 {tiles[position.0+1][position.1]} else {position_val};
-    let west = if position.1 > 0 {tiles[position.0][position.1-1]} else {position_val};
+    let north = tiles[position.0-1][position.1];
+    let east = tiles[position.0][position.1+1];
+    let south = tiles[position.0+1][position.1];
+    let west = tiles[position.0][position.1-1];
 
     let dir_1;
     let mut dir_2 = position;
-    if north != position_val && (north == '7' || north == 'F' || north == '|') {
+    if north == '7' || north == 'F' || north == '|' {
         dir_1 = (position.0-1, position.1);
-    } else if east != position_val && (east == '7' || east == 'J' || east == '-') {
+    } else if east == '7' || east == 'J' || east == '-' {
         dir_1 = (position.0, position.1+1);
     } else {
         dir_1 = (position.0+1, position.1);
         dir_2 = (position.0, position.1-1);
     }
 
-    if dir_2 == position && west != position_val && (west == 'F' || west == 'L' || west == '-') {
+    if dir_2 == position && (west == 'F' || west == 'L' || west == '-') {
         dir_2 = (position.0, position.1-1);
-    } else if dir_2 == position && south != position_val && (south == 'L' || south == 'J' || south == '|') {
+    } else if dir_2 == position && (south == 'L' || south == 'J' || south == '|') {
         dir_2 = (position.0+1, position.1);
     } else if dir_2 == position {
         dir_2 = (position.0, position.1+1);
@@ -114,49 +144,59 @@ fn get_directions(tiles: &Vec<Vec<char>>, position: (usize, usize)) -> ((usize, 
     (dir_1, dir_2)
 }
 
-fn get_next(tiles: &Vec<Vec<char>>, prev: (usize, usize), curr: (usize, usize)) -> (usize, usize) {
+fn get_next(tiles: &Vec<Vec<char>>, prev: (usize, usize), curr: (usize, usize)) -> ((usize, usize), usize) {
     let curr_val = tiles[curr.0][curr.1];
     match curr_val {
         '-' => if prev.1 < curr.1 {
-            (curr.0, curr.1+1)
+            ((curr.0, curr.1+1), 0)
         } else {
-            (curr.0, curr.1-1)
+            ((curr.0, curr.1-1), 2)
         },
         '|' => if prev.0 < curr.0 {
-            (curr.0+1, curr.1)
+            ((curr.0+1, curr.1), 1)
         } else {
-            (curr.0-1, curr.1)
+            ((curr.0-1, curr.1), 3)
         },
         'J' => if prev.1 < curr.1 {
-            println!("left");
-            (curr.0-1, curr.1)
+            //println!("left");
+            ((curr.0-1, curr.1), 3)
         } else {
-            println!("right");
-            (curr.0, curr.1-1)
+            //println!("right");
+            ((curr.0, curr.1-1), 2)
         },
         'F' => if prev.1 > curr.1 {
-            println!("left");
-            (curr.0+1, curr.1)
+            //println!("left");
+            ((curr.0+1, curr.1), 1)
         } else {
-            println!("right");
-            (curr.0, curr.1+1)
+            //println!("right");
+            ((curr.0, curr.1+1), 0)
         },
         'L' => if prev.0 < curr.0 {
-            println!("left");
-            (curr.0, curr.1+1)
+            //println!("left");
+            ((curr.0, curr.1+1), 0)
         } else {
-            println!("right");
-            (curr.0-1, curr.1)
+            //println!("right");
+            ((curr.0-1, curr.1), 3)
         },
         '7' => if prev.1 < curr.1 {
-            println!("right");
-            (curr.0+1, curr.1)
+            //println!("right");
+            ((curr.0+1, curr.1), 1)
         } else {
-            println!("left");
-            (curr.0, curr.1-1)
+            //println!("left");
+            ((curr.0, curr.1-1), 2)
         },
         _ => panic!("We got off the loop")
     }
+}
+
+fn get_next_left_of_dir(current: (usize, usize), dir: usize) -> (usize, usize) {
+    match dir {
+            1 => (current.0, current.1+1),
+            2=> (current.0+1, current.1),
+            3 => (current.0, current.1-1),
+            0 => (current.0-1, current.1),
+            _ => panic!("We went diagonal or something???")
+        }
 }
 
 fn main() {
